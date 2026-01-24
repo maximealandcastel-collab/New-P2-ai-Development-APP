d97a070153c1dfc0580b48e8b2c5d471a99e790c
golamshakib <golamshakib.h@gmail.com>
2026-01-24 18:08:59 +0600

    modified build.kts and run on iOs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,8 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        // Flag to enable support for the new language APIs (Flutter Local Notification)
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -22,6 +24,9 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.piertopier.app.p2p_fitness"
+        /// For Flutter Local Notification
+        multiDexEnabled = true
+
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -37,6 +42,11 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+/// For Flutter Local Notification
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
