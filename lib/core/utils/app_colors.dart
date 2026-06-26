6ef2a6e2f96b9cdf200491bc1f08453081a63c4b
tanvir-hridoy <ikramulhasantanvir@gmail.com>
2026-06-26 09:17:30 +0600

    Refactor app_routes.dart to remove unused routes and improve organization

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -7,44 +7,19 @@ class AppColors {
   static const Color primary = Color(0xFFFD7B00);
   static Color secondary = Color(0xFFe8e8e8);
 
-  // Gradient Colors
-  static const Gradient linearGradient = LinearGradient(
-    begin: Alignment(0.0, 0.0),
-    end: Alignment(0.707, -0.707),
-    colors: [Color(0xfffffa9e), Color(0xFFFAD0C4), Color(0xFFFAD0C4)],
-  );
-
   // Text Colors
   static const Color textPrimary = Color(0xFF000000);
   static const Color textSecondary = Color(0xFF7f7f7f);
-  static const Color textYellow = Color(0xffFFAB4C);
-  static const Color textGrey = Color(0xffA59F92);
   static const Color textWhite = Color(0xFFFFFFFF);
-  static const Color hintText = Color(0xFF93969C);
   static const Color colorE6E6E6 = Color(0xFFE6E6E6);
-
-  static const Color containerBg = Color(0xFFe0e0e0);
 
   // Background Colors
   static const Color backgroundLight = Color(0xFFF0F0F0);
   static const Color backgroundDark = Color(0xFF121212);
   static const Color primaryBackground = Color(0xFFFFFFFF);
 
-  // Surface Colors
-  static const Color surfaceLight = Color(0xFFE0E0E0);
-  static const Color surfaceDark = Color(0xFF2C2C2C);
-
-  // Container Colors
-  static const Color containerBackground = Color(0xFFD9D9D9);
-  static const Color containerBackground1 = Color(0xFFF9F9FB);
-
   // Utility Colors
   static const Color success = Color(0xFF4CAF50);
-  static const Color warning = Color(0xFFFFA726);
   static const Color error = Color(0xFFB20000);
   static const Color info = Color(0xFF0063F7);
-
-  /// textformfield border color
-
-  static const Color textFormFieldBorder = Color(0xFFD9D9D9);
 }
