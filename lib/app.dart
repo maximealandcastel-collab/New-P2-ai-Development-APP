bc2b035dd67220a22d0317ee46b5a30b3ccfcee5
devjaimul <jaimulislam7@gmail.com>
2026-06-27 12:30:37 +0600

    Update iOS project configuration and enhance app delegate for Flutter engine integration

--- lib/app.dart
+++ lib/app.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
+      useInheritedMediaQuery: true,
       builder: (context, child) => GetMaterialApp(
         theme: AppThemeData.themeData,
         debugShowCheckedModeBanner: false,
