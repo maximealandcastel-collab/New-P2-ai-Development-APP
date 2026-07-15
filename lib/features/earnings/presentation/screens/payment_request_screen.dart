ff732291352d713179f534ae568f1372636bf005
tanvirhridoy <ikramulhasantanvir@gmail.com>
2026-07-15 17:32:30 +0600

    Remove loading state indicator from save button in payment request screen

--- lib/features/earnings/presentation/screens/payment_request_screen.dart
+++ lib/features/earnings/presentation/screens/payment_request_screen.dart
@@ -44,7 +44,6 @@ class PaymentRequestScreen extends StatelessWidget {
           label: 'Save',
           backgroundColor: AppColors.primary,
           radius: 30.r,
-          isLoading: controller.requestState.isLoading,
         );
       }),
       bodyList: [
