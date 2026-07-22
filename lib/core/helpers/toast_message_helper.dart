ee59aad7b6e0a1ce63ff3981e0de3aac43da9608
tanvirhridoy <ikramulhasantanvir@gmail.com>
2026-07-22 16:09:33 +0600

    Add cooldown mechanism to toast message display to prevent spamming

--- lib/core/helpers/toast_message_helper.dart
+++ lib/core/helpers/toast_message_helper.dart
@@ -3,7 +3,16 @@ import 'package:get/get.dart';
 class ToastMessageHelper {
   ToastMessageHelper._();
 
+  static const Duration _cooldown = Duration(seconds: 3);
+  static DateTime? _lastShownAt;
+
   static void show(String message) {
+    final now = DateTime.now();
+    if (_lastShownAt != null && now.difference(_lastShownAt!) < _cooldown) {
+      return;
+    }
+
+    _lastShownAt = now;
     Get.snackbar(
       'Notification',
       message,
