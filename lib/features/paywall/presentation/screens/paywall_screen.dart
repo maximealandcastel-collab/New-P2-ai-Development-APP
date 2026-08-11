11e16c77163804cdf54718a91d7f0ffc935f17a4
Maxime Castel <maximealandcastel@gmail.com>
2026-08-11 16:54:48 -0500

    fix: use correct import paths (core/routes, api_client; remove image_path, dio_api_client)

--- lib/features/paywall/presentation/screens/paywall_screen.dart
+++ lib/features/paywall/presentation/screens/paywall_screen.dart
@@ -3,11 +3,10 @@ import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pler_to_pler_app/widgets/custom_text.dart';
 import 'package:pler_to_pler_app/core/utils/constants/app_colors.dart';
-import 'package:pler_to_pler_app/core/utils/constants/image_path.dart';
 import 'package:pler_to_pler_app/features/paywall/controllers/paywall_controller.dart';
 import 'package:pler_to_pler_app/features/nav_bar/presentation/screens/nav_bar.dart';
 import 'package:pler_to_pler_app/services/api_urls.dart';
-import 'package:pler_to_pler_app/services/network/dio_api_client.dart';
+import 'package:pler_to_pler_app/services/network/api_client.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class PaywallScreen extends StatelessWidget {
@@ -47,7 +46,7 @@ class PaywallScreen extends StatelessWidget {
 
                     // Logo
                     Image.asset(
-                      ImagePath.appLogo,
+                      'assets/images/app_logo.png',
                       height: 110.h,
                       errorBuilder: (context, error, stack) => Icon(
                         Icons.fitness_center,
@@ -714,12 +713,9 @@ class PaywallScreen extends StatelessWidget {
     loading.value = true;
     error.value = '';
     try {
-      final resp = await NetworkCaller.instance.postRequest(
-        url: ApiUrls.baseUrl + ApiUrls.adminBypass,
-        body: {'code': code},
-      );
+      final resp = await ApiClient.postData(ApiUrls.baseUrl + ApiUrls.adminBypass, {'code': code});
       loading.value = false;
-      if (resp.isSuccess) {
+      if (resp.statusCode == 200) {
         Get.back(); // close sheet
         Get.snackbar(
           '🔓 Admin Access Activated',
