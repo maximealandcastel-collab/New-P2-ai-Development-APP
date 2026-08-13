c2219bcf75492788fb52956328df9112a42b8907
Maxime Castel <maximealandcastel@gmail.com>
2026-08-13 15:36:58 -0500

    fix: correct app_routes import path to core/routes/app_routes.dart

--- lib/features/paywall/presentation/screens/paywall_screen.dart
+++ lib/features/paywall/presentation/screens/paywall_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pler_to_pler_app/widgets/custom_text.dart';
 import 'package:pler_to_pler_app/core/utils/constants/app_colors.dart';
 import 'package:pler_to_pler_app/features/paywall/controllers/paywall_controller.dart';
-import 'package:pler_to_pler_app/routes/app_routes.dart';
+import 'package:pler_to_pler_app/core/routes/app_routes.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:pler_to_pler_app/services/api_urls.dart';
 
