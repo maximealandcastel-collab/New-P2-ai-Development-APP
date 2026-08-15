09a72e35c6cf385d7e13208ddc564ad56d1b4444
Maxime Castel <maximealandcastel@gmail.com>
2026-08-14 20:46:23 -0500

    feat: add viewAsUser toggle to AdminModeService

--- lib/core/services/admin_mode_service.dart
+++ lib/core/services/admin_mode_service.dart
@@ -1,12 +1,21 @@
 import 'package:get/get.dart';
 
-/// Tracks whether the admin PIN has been activated this session.
-/// Registered on first PIN success; persists until app restart.
+/// Tracks whether admin mode is active and which view (admin / user) to show.
 class AdminModeService extends GetxController {
   static AdminModeService get to => Get.find();
 
   final _isAdmin = false.obs;
+  final _viewAsUser = false.obs;
+
   bool get isAdmin => _isAdmin.value;
 
+  /// When true the admin is browsing the app as a regular user.
+  bool get viewAsUser => _viewAsUser.value;
+
   void activate() => _isAdmin.value = true;
+
+  /// Flip between admin view and user view.
+  void toggleView() => _viewAsUser.value = !_viewAsUser.value;
+
+  void setViewAsUser(bool val) => _viewAsUser.value = val;
 }
