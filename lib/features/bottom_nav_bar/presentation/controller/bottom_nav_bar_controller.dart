18da2d4c4c14f6344f378ae8d2bc4ba07210426b
Maxime Castel <maximealandcastel@gmail.com>
2026-08-16 16:57:36 -0500

    fix: FAB shows user options (Find Trainer) in User mode, not trainer options

--- lib/features/bottom_nav_bar/presentation/controller/bottom_nav_bar_controller.dart
+++ lib/features/bottom_nav_bar/presentation/controller/bottom_nav_bar_controller.dart
@@ -50,10 +50,18 @@ class BottomNavBarController extends GetxController {
         : NavItemModel.userNavItems;
   }
 
-  List<NavFabModel> get fabItems =>
-      LoginController.to.isTrainer()
-          ? NavFabModel.trainerFabItems
-          : NavFabModel.userFabItems;
+  List<NavFabModel> get fabItems {
+    // When admin is viewing as a subscriber/user, always show user actions
+    // (Find Trainer, Add exercise plan) not trainer-only options.
+    final isAdmin = Get.isRegistered<AdminModeService>() &&
+        AdminModeService.to.isAdmin;
+    if (isAdmin && AdminModeService.to.viewAsUser) {
+      return NavFabModel.userFabItems;
+    }
+    return LoginController.to.isTrainer()
+        ? NavFabModel.trainerFabItems
+        : NavFabModel.userFabItems;
+  }
 
   static const int contentsTabIndex = 2;
 
