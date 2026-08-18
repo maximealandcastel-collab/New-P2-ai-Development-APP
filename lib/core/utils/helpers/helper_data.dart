9fa4dd78303aa20a8b4f4347c5156826c5d6bd6a
Maxime Castel <maximealandcastel@gmail.com>
2026-08-18 14:25:30 -0500

    chore: sync missing workspace file

---
+++ lib/core/utils/helpers/helper_data.dart
@@ -0,0 +1,19 @@
+class HelperData {
+
+  /// fake data
+  static final List<Map<String, dynamic>> notifications = [
+    {'name': 'Annette Black', 'message': 'Match request', 'date': DateTime.now(), 'type': 'request'},
+    {'name': 'Annette Black', 'message': 'Commented on your post', 'date': DateTime.now(), 'type': 'comment'},
+    {'name': 'Annette Black', 'message': 'Match request', 'date': DateTime.now().subtract(Duration(days: 1)), 'type': 'request'},
+  ];
+
+
+
+
+
+
+
+
+}
+
+
