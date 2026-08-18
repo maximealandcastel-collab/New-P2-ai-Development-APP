2c2a5718401e052b6a0d7bf4fe29cf030a179aa5
Maxime Castel <maximealandcastel@gmail.com>
2026-08-18 14:25:23 -0500

    chore: sync missing workspace file

---
+++ lib/core/utils/constants/icon_path.dart
@@ -0,0 +1,8 @@
+
+class IconPath{
+  IconPath._();
+  static const  String _basePath='assets/icons';
+  // static const String email = '$_basePath/Email.png';
+  static const String emailIcon = '$_basePath/email_icon.png';
+  static const String passwordIcon = '$_basePath/password_icon.png';
+}
