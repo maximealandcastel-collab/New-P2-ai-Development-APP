309b3e2f3cab0bc0e594973a5d9e5e4602b3e7ad
Maxime Castel <maximealandcastel@gmail.com>
2026-09-01 19:48:58 -0500

    Repair blank trainer client foundation

--- lib/features/trainer/clients/presentation/screens/clients_screen.dart
+++ lib/features/trainer/clients/presentation/screens/clients_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import '../../../../../core/utils/constants/app_colors.dart';
 import '../../../../../custom_assets/assets.gen.dart';
 import '../../../../../widgets/custom_app_bar.dart';
 import '../../../../../widgets/custom_container.dart';
@@ -9,55 +8,37 @@ import '../../../../../widgets/custom_scaffold.dart';
 import '../../../../../widgets/custom_text.dart';
 import '../../../../common/notification/presentation/screen/notification_screen.dart';
 
-class ClientsScreen extends StatefulWidget {
+class ClientsScreen extends StatelessWidget {
   const ClientsScreen({super.key});
-
-  @override
-  State<ClientsScreen> createState() => _ClientsScreenState();
-}
-
-class _ClientsScreenState extends State<ClientsScreen> {
-  final TextEditingController searchController = TextEditingController();
-  int selectedTab = 0; // 0: All clients, 1: Pending
-
 
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
       appBar: CustomAppBar(
         titleWidget: Padding(
-        padding: EdgeInsets.only(left: 12.w),
-        child: CustomText(
-          text: 'Clients',
-          fontSize: 17.sp,
-          fontWeight: FontWeight.w600,
+          padding: EdgeInsets.only(left: 12.w),
+          child: CustomText(
+            text: 'Clients',
+            fontSize: 17.sp,
+            fontWeight: FontWeight.w600,
+          ),
         ),
-      ),
         actions: [
           Padding(
             padding: EdgeInsets.only(right: 4.w),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Get.to(() => NotificationsScreen());
-                  },
-                  icon: Assets.icons.notification.svg(height: 42.r, width: 42.r),
-                ),
-              ],
+            child: IconButton(
+              onPressed: () => Get.to(() => NotificationsScreen()),
+              icon: Assets.icons.notification.svg(height: 42.r, width: 42.r),
             ),
           ),
         ],
       ),
-      // Wrapped the entire content in Padding for side-to-side consistency
       body: const Center(
         child: _EmptyClientsView(),
       ),
-
     );
   }
-
+}
 
 class _EmptyClientsView extends StatelessWidget {
   const _EmptyClientsView();
