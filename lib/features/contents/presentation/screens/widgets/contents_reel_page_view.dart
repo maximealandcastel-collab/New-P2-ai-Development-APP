d2eba3a0bee47b0b7b16db25319c1c2771f99d28
Maxime Castel <maximealandcastel@gmail.com>
2026-09-18 00:02:24 -0500

    Limit reel page preloading to one page

--- lib/features/contents/presentation/screens/widgets/contents_reel_page_view.dart
+++ lib/features/contents/presentation/screens/widgets/contents_reel_page_view.dart
@@ -90,7 +90,7 @@ class _ContentsReelPageViewState extends State<ContentsReelPageView> {
       child: PreloadPageView.builder(
         controller: controller.pageController,
         scrollDirection: Axis.vertical,
-        preloadPagesCount: 3,
+        preloadPagesCount: 1,
         physics: const AlwaysScrollableScrollPhysics(
           parent: PageScrollPhysics(),
         ),
